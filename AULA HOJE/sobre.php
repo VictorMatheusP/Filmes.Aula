6f9bb2aeaff8d181b6e031ada310e1582cee0a59
Victor M.P <108876854+VictorMatheusP@users.noreply.github.com>
2022-08-19 21:48:22 -0300

    Add files via upload

---
+++ AULA HOJE/sobre.php
@@ -0,0 +1,10 @@
+<?php include "header.php"; ?>
+<main>
+    <h1>Sobre a IMDB</h1>
+    <P>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae debitis alias accusantium, id doloribus tempora sapiente illo assumenda esse voluptatibus pariatur, amet optio veniam vel vero? Minima cupiditate accusantium optio?</P>
+    <P>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem sapiente, quo maxime explicabo vero voluptate fugit nobis? Atque vero eveniet, illo consequuntur, repellendus assumenda excepturi nobis, aliquam distinctio quidem voluptates.</P>
+</main>
+
+
+<?php include "footer.php"; ?>
+
